9c16a1c39be8458eef058eaac5a2fe8465ba4791
Asura0107 <mircoorosio@gmail.com>
2023-11-23 16:05:28 +0100

    U3-W1-D4

---
+++ assets/scrip.js
@@ -0,0 +1,26 @@
+fetch("https://mocki.io/v1/765b2daf-41d5-4e94-a0d5-abd918f57b8f")
+    .then(function (resp) { return resp.json(); })
+    .then(function (negozio) {
+    console.log(negozio);
+    negozio.forEach(function (stagione) {
+        //   console.log(stagione);
+        var Wear = /** @class */ (function () {
+            function Wear(_prezzoivainclusa, _saldo) {
+                this.prezzoivainclusa = _prezzoivainclusa;
+                this.saldo = _saldo;
+            }
+            Wear.prototype.getSaldoCapo = function () {
+                return (this.prezzoivainclusa - this.saldo) / 100;
+            };
+            Wear.prototype.getAcquistoCapo = function () {
+                var saldocapo = (this.prezzoivainclusa - this.saldo) / 100;
+                return this.prezzoivainclusa - saldocapo;
+            };
+            return Wear;
+        }());
+        var wear1 = new Wear(stagione.prezzoivainclusa, stagione.saldo);
+        console.log(stagione);
+        //   console.log(wear1.getSaldoCapo());
+        console.log("il prezzo di acquisto del capo \u00E8: ".concat(wear1.getAcquistoCapo()));
+    });
+});
---
+++ assets/scrip.ts
@@ -0,0 +1,30 @@
+fetch("https://mocki.io/v1/765b2daf-41d5-4e94-a0d5-abd918f57b8f")
+  .then((resp) => resp.json())
+  .then((negozio) => {
+    console.log(negozio);
+    negozio.forEach((stagione): any => {
+      //   console.log(stagione);
+      class Wear {
+        prezzoivainclusa: number;
+        saldo: number;
+
+        constructor(_prezzoivainclusa: number, _saldo: number) {
+          this.prezzoivainclusa = _prezzoivainclusa;
+          this.saldo = _saldo;
+        }
+        getSaldoCapo() {
+          return (this.prezzoivainclusa - this.saldo) / 100;
+        }
+        getAcquistoCapo() {
+          let saldocapo = (this.prezzoivainclusa - this.saldo) / 100;
+          return this.prezzoivainclusa - saldocapo;
+        }
+      }
+      let wear1 = new Wear(stagione.prezzoivainclusa, stagione.saldo);
+      console.log(stagione);
+      //   console.log(wear1.getSaldoCapo());
+      console.log(
+        `il prezzo di acquisto del capo è: ${wear1.getAcquistoCapo()}`
+      );
+    });
+  });
